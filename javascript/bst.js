51b19de580d0338c5257bfaca18c7878f9b0ec08
Julio Diaz <juliocm17@gmail.com>
2024-03-13 21:58:58 -0500

    Add BFS

--- javascript/bst.js
+++ javascript/bst.js
@@ -1,6 +1,36 @@
 // Online Javascript Editor for free
 // Write, Edit and Run your Javascript code using JS Online Compiler
 
+class Queue {
+    constructor() {
+        this.queue = [];
+    }
+    
+    enqueue(value) {
+        this.queue.unshift(value);
+    }
+    
+    dequeue() {
+        if(this.queue.length == 0) {
+            throw new Error('Queue is empty. Action not allowed.');
+        }
+        
+        return this.queue.pop();
+    }
+    
+    fetch() {
+        if(this.size == 0) {
+            return null;
+        }
+        
+        return this.queue[this.queue.length - 1];
+    }
+    
+    isEmpty() {
+        return this.queue.length == 0;
+    }
+}
+
 class Node {
     constructor(data){
         this.data = data;
@@ -59,7 +89,30 @@ class Node {
         if(this.right) this.right.dfsPostOrder();
         console.log(this.data);
     }
+    
+    bfs() {
+        const queue = new Queue();
+        queue.enqueue(this);
+        
+        while(!queue.isEmpty()) {
+            const node = queue.dequeue();
+            console.log(node.data);
+            
+            if(node.left) queue.enqueue(node.left)
+            if(node.right) queue.enqueue(node.right)
+        }
+    }
 }
+
+/*const queue = new Queue();
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(6);
+console.log(queue.fetch());
+queue.dequeue();
+console.log(queue.fetch());
+console.log(queue.isEmpty());
+queue.enqueue(12);*/
 
 const bts = new Node(11);
 bts.insert(8);
@@ -72,9 +125,12 @@ console.log(bts.contains(5))
 console.log(bts.contains(160))
 console.log(bts.contains(10))
 console.log(bts.contains(2))
+console.log('### DFS ###');
 console.log('PreOrder');
 bts.dfsPreOrder();
 console.log('InOrder');
 bts.dfsInOrder()
 console.log('PostOrder');
 bts.dfsPostOrder()
+console.log('### BFS ###');
+bts.bfs();
